8a98077afad857c0792441ac75d306f14ab9528a
studot <pedroherrera2783@hotmail.com>
2022-08-29 17:47:53 -0400

    Funcion playAround, decide quien gana

--- game.js
+++ game.js
@@ -13,4 +13,44 @@ function getComputerChoice(){
     return item;
 }
 
-console.log(getComputerChoice())
+
+function playAround(computerSelection, playerSelection){
+    //Seccion de Rock
+    if (computerSelection === "Rock" && playerSelection === "paper"){
+        console.log("You won! Paper beats rock.")
+    } 
+    else if (computerSelection === "Rock" && playerSelection === "scissor"){
+        console.log("You lost! Rock beats scissor.")
+    } 
+    else if (computerSelection === "Rock" && playerSelection === "rock"){
+        console.log("Tie!")
+    }
+
+    //Seccion de Paper
+    else if (computerSelection === "Paper" && playerSelection === "rock") {
+        console.log("You lost! Paper beats rock.")
+    } 
+    else if (computerSelection === "Paper" && playerSelection === "scissor") {
+        console.log("You won! Scissors beats paper.")
+    } 
+    else if (computerSelection === "Paper" && playerSelection === "paper") {
+        console.log("Tie!")
+    } 
+
+    //Seccion de Scissor 
+    else if (computerSelection === "Scissor" && playerSelection === "rock") {
+        console.log("You lost! Paper beats rock.")
+    } 
+    else if (computerSelection === "Scissor" && playerSelection === "paper") {
+        console.log("You won! Scissors beats paper.")
+    } 
+    else if (computerSelection === "Scissor" && playerSelection === "scissor") {
+        console.log("Tie!")
+    }
+}
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Choose! Rock, Paper, Scissor: ").toLowerCase();
+
+playAround(computerSelection, playerSelection);
